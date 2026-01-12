6e9d7e9af16c60fa5d98a9aaddaebde5bc9d7672
fadrianlh <adrianhhfung@gmail.com>
2026-01-11 22:09:13 -0800

    implemented docker for both backend and frontend, used docker compose to combine both networks. Changed backend host address to accept all accesses from other containers.

--- backend/app.py
+++ backend/app.py
@@ -660,4 +660,4 @@ if __name__ == '__main__':
         db.create_all()
         print('Database tables created')
     print('Starting Flask server on port 5000...')
-    app.run(debug=True, port=5000, host='127.0.0.1')
+    app.run(debug=True, port=5000, host='0.0.0.0')
